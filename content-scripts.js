f3c6ead7cf1fe44bf11cc088a75b42a6e483d572
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-10-20 10:30:39 +0800

    complete

--- content-scripts.js
+++ content-scripts.js
@@ -1,28 +1,487 @@
-// This code will add a button to the specified pages
-console.log('content_scripts loaded');
+var pipsToPipolConverter = [
+    {
+      "pipsId": 1,
+      "pipolId": 34
+    },
+    {
+      "pipsId": 2,
+      "pipolId": 35
+    },
+    {
+      "pipsId": 3,
+      "pipolId": 39
+    },
+    {
+      "pipsId": 4,
+      "pipolId": 60
+    },
+    {
+      "pipsId": 5,
+      "pipolId": 11
+    },
+    {
+      "pipsId": 6,
+      "pipolId": 18
+    },
+    {
+      "pipsId": 7,
+      "pipolId": 37
+    },
+    {
+      "pipsId": 8,
+      "pipolId": 55
+    },
+    {
+      "pipsId": 9,
+      "pipolId": 62
+    },
+    {
+      "pipsId": 10,
+      "pipolId": 10
+    },
+    {
+      "pipsId": 11,
+      "pipolId": 17
+    },
+    {
+      "pipsId": 12,
+      "pipolId": 54
+    },
+    {
+      "pipsId": 13,
+      "pipolId": 59
+    },
+    {
+      "pipsId": 14,
+      "pipolId": 75
+    },
+    {
+      "pipsId": 15,
+      "pipolId": 77
+    },
+    {
+      "pipsId": 16,
+      "pipolId": 8
+    },
+    {
+      "pipsId": 17,
+      "pipolId": 12
+    },
+    {
+      "pipsId": 18,
+      "pipolId": 24
+    },
+    {
+      "pipsId": 19,
+      "pipolId": 40
+    },
+    {
+      "pipsId": 20,
+      "pipolId": 61
+    },
+    {
+      "pipsId": 21,
+      "pipolId": 63
+    },
+    {
+      "pipsId": 22,
+      "pipolId": 5
+    },
+    {
+      "pipsId": 23,
+      "pipolId": 19
+    },
+    {
+      "pipsId": 24,
+      "pipolId": 20
+    },
+    {
+      "pipsId": 25,
+      "pipolId": 23
+    },
+    {
+      "pipsId": 26,
+      "pipolId": 46
+    },
+    {
+      "pipsId": 27,
+      "pipolId": 68
+    },
+    {
+      "pipsId": 28,
+      "pipolId": 4
+    },
+    {
+      "pipsId": 29,
+      "pipolId": 6
+    },
+    {
+      "pipsId": 30,
+      "pipolId": 22
+    },
+    {
+      "pipsId": 31,
+      "pipolId": 36
+    },
+    {
+      "pipsId": 32,
+      "pipolId": 51
+    },
+    {
+      "pipsId": 33,
+      "pipolId": 32
+    },
+    {
+      "pipsId": 34,
+      "pipolId": 15
+    },
+    {
+      "pipsId": 35,
+      "pipolId": 25
+    },
+    {
+      "pipsId": 36,
+      "pipolId": 52
+    },
+    {
+      "pipsId": 37,
+      "pipolId": 67
+    },
+    {
+      "pipsId": 38,
+      "pipolId": 31
+    },
+    {
+      "pipsId": 39,
+      "pipolId": 43
+    },
+    {
+      "pipsId": 40,
+      "pipolId": 53
+    },
+    {
+      "pipsId": 41,
+      "pipolId": 65
+    },
+    {
+      "pipsId": 42,
+      "pipolId": 70
+    },
+    {
+      "pipsId": 43,
+      "pipolId": 14
+    },
+    {
+      "pipsId": 44,
+      "pipolId": 78
+    },
+    {
+      "pipsId": 45,
+      "pipolId": 79
+    },
+    {
+      "pipsId": 46,
+      "pipolId": 80
+    },
+    {
+      "pipsId": 47,
+      "pipolId": 16
+    },
+    {
+      "pipsId": 48,
+      "pipolId": 21
+    },
+    {
+      "pipsId": 49,
+      "pipolId": 41
+    },
+    {
+      "pipsId": 50,
+      "pipolId": 48
+    },
+    {
+      "pipsId": 51,
+      "pipolId": 49
+    },
+    {
+      "pipsId": 52,
+      "pipolId": 28
+    },
+    {
+      "pipsId": 53,
+      "pipolId": 29
+    },
+    {
+      "pipsId": 54,
+      "pipolId": 30
+    },
+    {
+      "pipsId": 55,
+      "pipolId": 26
+    },
+    {
+      "pipsId": 56,
+      "pipolId": 88
+    },
+    {
+      "pipsId": 57,
+      "pipolId": 27
+    },
+    {
+      "pipsId": 58,
+      "pipolId": 69
+    },
+    {
+      "pipsId": 59,
+      "pipolId": 71
+    },
+    {
+      "pipsId": 60,
+      "pipolId": 66
+    },
+    {
+      "pipsId": 61,
+      "pipolId": 47
+    },
+    {
+      "pipsId": 62,
+      "pipolId": 1
+    },
+    {
+      "pipsId": 63,
+      "pipolId": 7
+    },
+    {
+      "pipsId": 64,
+      "pipolId": 13
+    },
+    {
+      "pipsId": 65,
+      "pipolId": 33
+    },
+    {
+      "pipsId": 66,
+      "pipolId": 38
+    },
+    {
+      "pipsId": 67,
+      "pipolId": 50
+    },
+    {
+      "pipsId": 68,
+      "pipolId": 2
+    },
+    {
+      "pipsId": 69,
+      "pipolId": 3
+    },
+    {
+      "pipsId": 70,
+      "pipolId": 82
+    },
+    {
+      "pipsId": 71,
+      "pipolId": 73
+    },
+    {
+      "pipsId": 72,
+      "pipolId": 74
+    },
+    {
+      "pipsId": 73,
+      "pipolId": 9
+    },
+    {
+      "pipsId": 74,
+      "pipolId": 42
+    },
+    {
+      "pipsId": 75,
+      "pipolId": 44
+    },
+    {
+      "pipsId": 76,
+      "pipolId": 72
+    },
+    {
+      "pipsId": 77,
+      "pipolId": 76
+    },
+    {
+      "pipsId": 78,
+      "pipolId": 45
+    },
+    {
+      "pipsId": 79,
+      "pipolId": 56
+    },
+    {
+      "pipsId": 80,
+      "pipolId": 57
+    },
+    {
+      "pipsId": 81,
+      "pipolId": 58
+    },
+    {
+      "pipsId": 82,
+      "pipolId": 64
+    },
+    {
+      "pipsId": 83,
+      "pipolId": 84
+    },
+    {
+      "pipsId": 84,
+      "pipolId": 85
+    },
+    {
+      "pipsId": 85,
+      "pipolId": 86
+    },
+    {
+      "pipsId": 86,
+      "pipolId": 87
+    },
+    {
+      "pipsId": 87,
+      "pipolId": 81
+    },
+    {
+      "pipsId": 88,
+      "pipolId": 83
+    }
+];
 
-function findDivWithTextContent(targetText) {
-    const divsWithText = Array.from(document.querySelectorAll('div')); // Select all div elements
-
-    const matchingDivs = divsWithText.filter(div => div.textContent.includes(targetText)); // Filter divs with specific text
-
-    return matchingDivs;
+function generatePipolId(
+    pipsId
+) {
+    return pipsToPipolConverter.find((el) => el.pipsId == pipsId);
 }
 
-function addButton() {
+async function pasteProvincialBreakdown() {
+    var buttonTrigger = document.getElementById('pasteProvincialBreakdown');
+    buttonTrigger.innerHTML = 'PASTING...';
+
+    try {
+        var clipboardContent = await navigator.clipboard.readText();
+
+        var valueFromClipboard = JSON.parse(clipboardContent);
+    
+        if (! Array.isArray(valueFromClipboard)) {
+            alert('Value is invalid: Not an array');
+        }
+    
+        // validate if format is correct by inspecting the first item
+        var firstItem = valueFromClipboard[0];
+    
+        if (!firstItem.hasOwnProperty('province_id') 
+            || !firstItem.hasOwnProperty('y2022') 
+            || !firstItem.hasOwnProperty('y2023') 
+            || !firstItem.hasOwnProperty('y2024') 
+            || !firstItem.hasOwnProperty('y2025') 
+            || !firstItem.hasOwnProperty('y2026') 
+            || !firstItem.hasOwnProperty('y2027') 
+            || !firstItem.hasOwnProperty('y2028') 
+            || !firstItem.hasOwnProperty('y2029')) {
+            return alert('Data is INVALID. Please check if you have copied the data from provincial investments');
+        }
+        
+        var errors = [];
+
+        for (item of valueFromClipboard) {
+            var matchedId = generatePipolId(item.province_id);
+    
+            if (!matchedId) {
+                console.log('province id not found');
+                continue;
+            }
+            
+            var pipolId = matchedId.pipolId;
+    
+            var y2022 = document.getElementById('pb_2022_' + pipolId);
+            var y2023 = document.getElementById('pb_2023_' + pipolId);
+            var y2024 = document.getElementById('pb_2024_' + pipolId);
+            var y2025 = document.getElementById('pb_2025_' + pipolId);
+            var y2026 = document.getElementById('pb_2026_' + pipolId);
+            var y2027 = document.getElementById('pb_2027_' + pipolId);
+            var y2028 = document.getElementById('pb_2028_' + pipolId);
+            var y2029 = document.getElementById('pb_cy_' + pipolId);
+
+            if (!y2022) {
+                // if not found add province to errors
+                errors.push(item.province.label);
+            }
+            
+            if (y2022 && !y2022.readOnly) {            
+                y2022.value = item.y2022;
+                y2022.focus();
+            }
+    
+            if (y2023 && !y2023.readOnly) {
+                y2023.value = item.y2023;
+                y2023.focus();
+            }
+    
+            if (y2024 && !y2024.readOnly) {
+                y2024.value = item.y2024;
+                y2024.focus();
+            }
+            
+            if (y2025 && !y2025.readOnly) {
+                y2025.value = item.y2025;
+                y2025.focus();
+            }
+            
+            if (y2026 && !y2026.readOnly) {
+                y2026.value = item.y2026;
+                y2026.focus();
+            }
+            
+            if (y2027 && !y2027.readOnly) {
+                y2027.value = item.y2027;
+                y2027.focus();
+            }
+            
+            if (y2028 && !y2028.readOnly) {
+                
+                y2028.value = item.y2028;
+                y2028.focus();
+            }
+            
+            if (y2029 && !y2029.readOnly) {
+                y2029.value = item.y2029;
+                y2029.focus();
+            }
+    
+            console.log('Successfully copied ' + item.province.label);
+        }
+        
+        if (errors.length > 0) {
+            alert('Successfully copied values except the following: ' + errors.join(', '));
+        } else {
+            alert('Successfully copied value');
+        }
+
+        buttonTrigger.innerHTML = 'PASTE PROVINCIAL INVESTMENTS';
+    } catch (err) {
+        buttonTrigger.innerHTML = 'PASTE PROVINCIAL INVESTMENTS';
+
+        alert('Something went wrong. Verify if you have copied the correct data. Error: ' + err.toString());
+    }
+   
+}
+
+var divprovincialbreakdown = document.getElementById('divprovincialbreakdown');
+
+if (divprovincialbreakdown) {
     const button = document.createElement('button');
-    button.innerText = 'Click Me';
-    button.addEventListener('click', function() {
-        alert('Button Clicked!');
-    });
-
-    const targetText = 'Regional Breakdown';
-    const matchingDivs = findDivWithTextContent(targetText);
-
-    // Assuming you want to add the button to the body of the page
-    // const targetElement = document.getElementById('targetElement');
-    matchingDivs[0].appendChild(button);
+    button.id = 'pasteProvincialBreakdown';
+    button.type = 'button';
+    button.classList.add('btn-primary');
+    button.classList.add('btn');
+    button.classList.add('btn-lg');
+    button.innerHTML = 'PASTE PROVINCIAL INVESTMENTS';
+    button.onclick = pasteProvincialBreakdown;
+    divprovincialbreakdown.appendChild(button);
 }
-
-// Call the function to add the button
-addButton();
